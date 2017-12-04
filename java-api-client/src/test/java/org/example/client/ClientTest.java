5d20044070002f7df39dc33b798bf3f337634910
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-12-04 12:02:14 +0100

    Add java-api-client example

---
+++ java-api-client/src/test/java/org/example/client/ClientTest.java
@@ -0,0 +1,30 @@
+package org.example.client;
+
+import com.cloudesire.platform.apiclient.CloudesireClient;
+import com.cloudesire.platform.apiclient.CloudesireClientCallExecutor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.liberologico.cloudesire.cmw.model.dto.ProductDTO;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ClientTest
+{
+    @Test
+    public void defaultTest()
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure( DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false );
+        CloudesireClientCallExecutor exec = new CloudesireClientCallExecutor( mapper );
+
+        CloudesireClient.Builder builder = new CloudesireClient.Builder()
+                .setBaseUrl( "https://backend.cloudesire.com/api/" )
+                .setMapper( mapper );
+        CloudesireClient client = builder.build();
+        List<ProductDTO> products = exec.execute( client.getProductApi().getAll() );
+        assertNotNull( products );
+    }
+}
